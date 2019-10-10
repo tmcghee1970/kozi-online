9bda537a50d719eca0a45649e3ffc45aa9de20cb
Tyrone McGhee <tmcghee1970@gmail.com>
2019-10-09 23:07:25 -0400

    Added authors service and component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthorsService } from "./authors.service";
 import { CoursesService } from "./courses.service";
 import { CoursesComponent } from "./courses.component";
 import { BrowserModule } from "@angular/platform-browser";
@@ -5,11 +6,17 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CourseComponent } from "./course/course.component";
+import { AuthorsComponent } from "./authors/authors.component";
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent],
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
+  ],
   imports: [BrowserModule],
-  providers: [CoursesService],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
