ce87df89647406a678ea65d4136962fe43eed307
Tyrone McGhee <tmcghee1970@gmail.com>
2019-10-09 22:50:02 -0400

    Added courses and email services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
+import { CoursesService } from "./courses.service";
 import { CoursesComponent } from "./courses.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { CourseComponent } from './course/course.component';
+import { CourseComponent } from "./course/course.component";
 
 @NgModule({
   declarations: [AppComponent, CoursesComponent, CourseComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
